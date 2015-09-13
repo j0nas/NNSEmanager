0f533061273940d0677b60db8987acaa2ae90a24
j0nas <jonas.jensen@msn.com>
2015-09-13 10:22:07 +0200

    Root index lists all active tenants

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('tenant/list');
+    res.render('tenant/list', {url: 'active'});
 });
 
 module.exports = router;
